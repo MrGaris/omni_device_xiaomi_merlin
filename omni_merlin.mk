3ba39b5ea0d77df671900d318a5a456246fb3077
MrGaris <standoffplay94@gmail.com>
2026-05-29 15:21:45 +0300

    Refactor omni_merlin.mk for device configuration

--- omni_merlin.mk
+++ omni_merlin.mk
@@ -16,41 +16,16 @@
 # limitations under the License.
 #
 
-# PBRP-10.0 EDITS - START
-
-PRODUCT_RELEASE_NAME := merlin
-# PRODUCT_USE_DYNAMIC_PARTITIONS := true
 DEVICE_PATH := device/xiaomi/merlin
 
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
-
-# Inherit from our custom product configuration
 $(call inherit-product, vendor/pb/config/common.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 
 PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,$(LOCAL_PATH)/recovery/root,recovery/root)
 
-# Inherit PBRP stuff.
-$(call inherit-product, vendor/pb/config/common.mk)
-
-# PBRP-10.0 EDITS - END
-
-# Specify phone tech before including full_phone
-# $(call inherit-product, vendor/omni/config/gsm.mk)
-
-# Inherit some common Omni stuff.
-# $(call inherit-product, vendor/omni/config/common.mk)
-# $(call inherit-product, build/target/product/embedded.mk)
-
-# Inherit Telephony packages
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-
-# Inherit language packages
-$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
-
-# Inherit 64bit support
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-
-# Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := merlin
 PRODUCT_NAME := omni_merlin
 PRODUCT_BRAND := xiaomi
